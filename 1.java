93a47552d8ed8fcb5d3198a2e26e949d68b8517f
se-cancle <1664815002@qq.com>
2021-01-19 22:23:36 +0800

    在同一个类中,分别定义求两个整数的方法 和 三个小数之和的方法

---
+++ 1.java
@@ -0,0 +1,80 @@
+public class test {
+    public static void main(String[] args) {
+//在同一个类中,分别定义求两个整数的方法 和 三个小数之和的方法。 并执行代码，求出结果
+    Sum sum=new Sum();
+    sum.sum1();
+    sum.sum2();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int result=max(10,20);
+//        System.out.println(result);
+//
+//        int result1=min(10,20);
+//        System.out.println(result1);
+//
+//        double result2=max(10.0,20.0);
+//        System.out.println(result2);
+//
+//        double result3=min(10.0,20.0);
+//        System.out.println(result3);
+//
+//        double result4=
+//    }
+//
+//    public static double max(double v, double v1) {
+//        if(v>v1){
+//            return v;
+//        }else{
+//            return v1;
+//        }
+//    }
+//
+//    public static double min(double v, double v1) {
+//        if(v<v1){
+//            return v;
+//        }else{
+//            return v1;
+//        }
+//    }
+//
+//    public static int min(int i, int i1) {
+//        if(i<i1){
+//            return i;
+//        }else{
+//            return i1;
+//        }
+//    }
+//
+//    public static int max(int i, int i1) {
+//        if(i>i1){
+//            return i;
+//        }else{
+//            return i1;
+//        }
+//    }
+
---
+++ Sum.java
@@ -0,0 +1,17 @@
+public class Sum {
+
+        public int a=1;
+        public int b=2;
+        public double c=1.0;
+        public double d=2.0;
+        public double e=3.0;
+
+        public void sum1(){
+            System.out.println(a+b);
+        }
+        public void sum2(){
+            System.out.println(c+d+e);
+        }
+    }
+
+
